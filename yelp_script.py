5be1f11ec96372bff4a48e2ccceb12703ef8d0ce
Weng Yu <yuweng95@gmail.com>
2020-03-22 17:27:25 -0400

    friend

--- yelp_script.py
+++ yelp_script.py
@@ -111,9 +111,9 @@ def split_friend(id, column_name, table_name):
             col.append(i[0])
         data = list(map(list, data))
         data = pd.DataFrame(data, columns=col)
-        length = len(data) % 10
-        for i in range(1, length):
-            split_data = (data.set_index([id])[column_name][i*10-10:i*10]
+        length = len(data) 
+        for i in range(0, length):
+            split_data = (data.set_index([id])[column_name][i]
                    .str.split(',', expand=True)
                    .stack()
                    .reset_index(level=1, drop=True)
@@ -124,12 +124,12 @@ def split_friend(id, column_name, table_name):
             print(gc.collect())
             sleep(5)
 
-        split_data = (data.set_index([id])[column_name][length * 10:-1]
-                      .str.split(',', expand=True)
-                      .stack()
-                      .reset_index(level=1, drop=True)
-                      .reset_index(name=column_name))
-        insert_data("Friends", split_data)
+        # split_data = (data.set_index([id])[column_name][length * 10:-1]
+        #               .str.split(',', expand=True)
+        #               .stack()
+        #               .reset_index(level=1, drop=True)
+        #               .reset_index(name=column_name))
+        # insert_data("Friends", split_data)
 
 
 # set up python on server
